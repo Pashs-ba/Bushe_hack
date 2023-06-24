f3c0aa67634a90420fcd18e6e3963aa5eb79d5fe
AlanTheKnight <51376126+AlanTheKnight@users.noreply.github.com>
2023-06-24 12:54:50 +0300

    Some changes

--- frontend/env.d.ts
+++ frontend/env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  VITE_APP_TELEGRAM_BOT_TOKEN: string
+  VITE_APP_TELEGRAM_BOT_NAME: string
+  VITE_APP_TELEGRAM_AUTH_CALLBACK_URL: string
+}
+
+declare const __APP_VERSION__: string
---
+++ frontend/src/utils/auth.ts
@@ -0,0 +1,9 @@
+export interface User {
+  auth_date: number
+  first_name: string
+  hash: string
+  id: number
+  last_name?: string
+  username: string
+  photo_url?: string
+}
